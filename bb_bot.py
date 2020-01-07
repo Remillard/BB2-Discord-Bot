9ddc453f2b39d8e3ec4147fdb1721dc2def165ed
Mark Norton <mark.norton@viavisolutions.com>
2020-01-06 22:37:45 -0600

    Trivia portion of the bot working now
    Vast amount of YAML editing... whew

--- bb_bot.py
+++ bb_bot.py
@@ -1,19 +1,59 @@
 #! python3
 # See https://realpython.com/how-to-make-a-discord-bot-python/
 import os
-import discord
-import logging
-
-logging.basicConfig(level=logging.INFO)
-
+import argparse
+import bb_trivia
 from dotenv import load_dotenv
+from discord.ext import commands
 
-load_dotenv()
-token = os.getenv("DISCORD_TOKEN")
-client = discord.Client()
 
-@client.event
+##################
+def dump_context(ctx):
+    print("Message Parameters Dump")
+    print("=======================")
+    print(f"Message Author Name: {ctx.author.name}")
+    print(f"Message Author Discrminator: {ctx.author.discriminator}")
+    print(f"Message Author is a Bot: {ctx.author.bot}")
+    print(f"Message Content: {ctx.message.content}")
+    print(f"Message was in Guild: {ctx.guild}")
+    print(f"Message was in channel: {ctx.channel}")
+    print(f"Message printed at: {ctx.message.created_at}")
+##################
+
+################################################################################
+# Read the invocation arguments and initialize the various files.
+################################################################################
+parser = argparse.ArgumentParser(
+    prog="bb_trivia", description="Prints out a random Blood Bowl trivia fact."
+)
+parser.add_argument("filename", help="The trivia data file (YAML format).")
+args = parser.parse_args()
+trivia_file = bb_trivia.TriviaFile(args.filename)
+
+
+
+################################################################################
+# Discord Bot Commands
+################################################################################
+bot = commands.Bot(command_prefix="!")
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f"{bot.user.name} has connected to Discord.")
 
-client.run(token)
+@bot.command(name="trivia", help="Responds with a randomly selected bit of trivia.")
+async def trivia(ctx):
+    dump_context(ctx)
+    print(trivia_file)
+    await ctx.send(trivia_file)
+
+
+################################################################################
+# Initialize and launch the bot
+# Token is obtained from the environment
+################################################################################
+load_dotenv()
+token = os.getenv("BBB_DISCORD_TOKEN")
+print(f"Proceeding with BloodBowlBot Token: {token}")
+bot.run(token)
+
+
