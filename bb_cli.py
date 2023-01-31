6f279599e7df9b7243ef540b8f2efb980f5f8854
Mark Norton <mark.norton@viavisolutions.com>
2023-01-31 14:17:22 -0600

    Typer variation of the CLI

--- bb_cli.py
+++ bb_cli.py
@@ -3,57 +3,40 @@
 # Command line interface into the database
 ################################################################################
 import os
-import argparse
+import typer
 
 import models
 import engine
 
 ################################################################################
-def main():
-    """
-    Main command line interface entry point.
-    """
-    parser = argparse.ArgumentParser(
-        prog="bb_cli",
-        description="An interface into the Blood Bowl tournament database.",
-    )
-    parser.add_argument("filename", help="The tournament SQLite3 database file.")
-    parser.add_argument(
-        "--initialize",
-        action="store_true",
-        help="""Deletes the specified database if it exists and recreates the
-        database initial state including enumerated tables.""",
-    )
-    parser.add_argument(
-        "--add_coach",
-        help='''Should have a parameter of a quoted string of a comma-separated
-        list of values in the order: Discord Name, BB2 Coach Name, BB3 Coach Name.
-        Example: "Remi#2160, Remillard, Remi"'''
-    )
-    args = parser.parse_args()
+# Main CLI interface entry point.
+cli = typer.Typer()
 
-    if args.initialize:
-        if os.path.exists(args.filename):
-            answer = input(
-                f"Are you VERY sure you wish to delete {args.filename} and recreate?  Type YES to continue, any other input to exit: "
-            )
-            if answer == "YES":
-                print("Deleting specified database.")
-                os.remove(args.filename)
-            else:
-                print("Exiting.")
-                quit()
-    
-        my_engine = engine.initialize_engine(args.filename)
-        engine.initialize_tables(my_engine)
-        engine.initialize_race_table(my_engine)
-    else:
-        my_engine = engine.initialize_engine(args.filename)
-        engine.initialize_tables(my_engine)
 
-    if args.add_coach:
-        engine.add_coach(my_engine, args.add_coach)
+@cli.command()
+def initialize(filename: str):
+    if os.path.exists(filename):
+        answer = input(
+            f"Are you VERY sure you wish to delete {filename} and recreate?  Type YES to continue, any other input to exit: "
+        )
+        if answer == "YES":
+            print("Deleting specified database.")
+            os.remove(filename)
+        else:
+            print("Exiting.")
+            quit()
+
+    my_engine = engine.initialize_engine(filename)
+    engine.initialize_tables(my_engine)
+    engine.initialize_race_table(my_engine)
+
+
+@cli.command()
+def add_coach(filename: str, coach_csv: str):
+    my_engine = engine.initialize_engine(filename)
+    engine.initialize_tables(my_engine)
+    engine.add_coach(my_engine, coach_csv)
 
 
 if __name__ == "__main__":
-    main()
+    cli()
